89afbb10c5952f49f0ceec288b79bfe6572ebac5
adnan14 <a_spahic@hotmail.com>
2015-02-01 13:30:18 +0100

    Server & Socket

---
+++ Server.java
@@ -0,0 +1,67 @@
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.util.Scanner;
+
+public class Server {
+	// nesto kao adresa ili otvor gdje cekamo konekciju,
+	// rade od 0 do cc.65000,
+	// koristiti portove iznad 1500
+	public static final int port = 1728;
+
+	/**
+	 * @throws IOException
+	 * 
+	 */
+
+	public static void startServer() {
+		try {
+			Scanner scen = new Scanner(System.in);
+			// socket koji ceka da se neko na njega spoji
+			ServerSocket server = new ServerSocket(port);
+			while (true) {
+
+				System.out.println("waiting");
+				Socket client = server.accept();
+				SocketRW sc = new SocketRW(client.getInputStream(),
+						client.getOutputStream());
+				while(true){
+					System.out.print("Server: ");
+					
+					
+					String userInput = scen.nextLine();
+
+					sc.send(userInput);
+					if(userInput.equals("quit"))
+						break;
+					String message = sc.recieve();
+					System.out.println("Client: "+message);
+					
+					if(userInput.equals("quit") || message.equals("quit")){
+						break;
+					}
+				}
+				
+				
+
+				client.close();
+				// accept vraca new Socket
+				// kada upisemo accept nista ispod ove
+				// linije nece se izvrsiti dok se ne desi konekcija
+
+				System.out.println("Gotovo");
+
+			}
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+
+	public static void main(String[] args) {
+		startServer();
+	}
+
+}
---
+++ SocketConnector.java
@@ -0,0 +1,67 @@
+import java.io.BufferedInputStream;
+import java.io.BufferedReader;
+import java.io.DataInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.OutputStream;
+import java.net.Socket;
+import java.net.UnknownHostException;
+import java.util.Scanner;
+
+import javax.swing.plaf.SliderUI;
+
+public class SocketConnector {
+
+	// umjesto 127.0.0.1. moze i localHost
+	public static final String serverAdress = "127.0.0.1";
+	// port mora biti isti ako i kod servera!!
+	public static final int port = 1728;
+
+	/**
+	 * 
+	 * @throws UnknownHostException
+	 * @throws IOException
+	 */
+
+	private static void connectToServer() throws UnknownHostException,
+			IOException {
+
+		Socket client = new Socket(serverAdress, port);
+
+		SocketRW sc = new SocketRW(client.getInputStream(),
+				client.getOutputStream());
+		Scanner scen = new Scanner(System.in);
+		
+		while (true) {
+			String message = sc.recieve();
+			System.out.println("Server: "+message);
+			if(message.equals("quit"))
+				break;
+			System.out.print("Client: ");
+			String userInput = scen.nextLine();
+
+			sc.send(userInput);
+			if (userInput.equals("quit")) {
+				break;
+			}
+		}
+
+		System.out.println("\nGotovo");
+		client.close();
+
+	}
+
+	public static void main(String[] args) {
+		try {
+			connectToServer();
+		} catch (UnknownHostException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+
+}
